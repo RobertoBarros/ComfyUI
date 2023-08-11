c587d879f0e28af320e8d03c6e9396b86d3f14d4
Roberto Barros <roberto.f.barros@gmail.com>
2023-08-11 15:24:14 -0300

    copy new model-list to manager

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git submodule update --init --recursive
+wget -O './custom_nodes/ComfyUI-Manager/model-list.json' https://gist.githubusercontent.com/RobertoBarros/5e20ae05a18cb3ede672f763c62dc60b/raw/82f8d240810331d369a2478415cbd4968af55486/model-list.json
 source ../stable-diffusion-webui/venv/bin/activate
 python3 main.py --port 3000
-
