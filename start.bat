e878c91434bd99f9b3c1e80f7995b08f66231b49
Roberto Barros <roberto.f.barros@gmail.com>
2023-08-11 21:21:49 -0300

    change link

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 git submodule update --init --recursive
 
-curl -o custom_nodes/ComfyUI-Manager/model-list.json https://gist.githubusercontent.com/RobertoBarros/5e20ae05a18cb3ede672f763c62dc60b/raw/fdaf1f4c85694e0610995570f09a495566c40a99/model-list.json
+curl -o custom_nodes/ComfyUI-Manager/model-list.json https://raw.githubusercontent.com/RobertoBarros/ComfyUI/master/model_list.json
 
 python3 main.py --port 3000
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git submodule update --init --recursive
 
-wget -O ./custom_nodes/ComfyUI-Manager/model-list.json https://gist.githubusercontent.com/RobertoBarros/5e20ae05a18cb3ede672f763c62dc60b/raw/fdaf1f4c85694e0610995570f09a495566c40a99/model-list.json
+wget -O ./custom_nodes/ComfyUI-Manager/model-list.json https://raw.githubusercontent.com/RobertoBarros/ComfyUI/master/model_list.json
 
 source ../stable-diffusion-webui/venv/bin/activate
 
